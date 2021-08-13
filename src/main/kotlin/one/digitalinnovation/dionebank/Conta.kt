0f023fe80949d92b8901a5e6b2e6ee0c19eda17c
lourenco <victorslourenco@gmail.com>
2021-08-13 19:53:03 -0300

    create class conta

---
+++ src/main/kotlin/one/digitalinnovation/dionebank/Conta.kt
@@ -0,0 +1,16 @@
+package one.digitalinnovation.dionebank
+
+import java.math.BigDecimal
+
+class Conta (
+    val agencia: String
+    val conta: String
+    val saldo: BigDecimal
+) {
+    fun deposito(valor: BigDecimal)
+
+}
+
+{
+    fun saque(valor: BigDecimal)
+}
